f700f5c29d40df8a8443349182ff4b21f9c697b7
gonzav90 <gonzalovolpe@hotmail.com>
2022-08-31 15:33:50 -0300

    render object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-// El componente App es el padre de:
-// - Cabecera
-// - Listado
-// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
-// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
-// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+import { useState } from "react";
+import Cabecera from "./components/Cabecera"
+import Listado from "./components/Listado"
 
 function App() {
+   
 
   return (
     <div className="App">
@@ -14,5 +12,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
